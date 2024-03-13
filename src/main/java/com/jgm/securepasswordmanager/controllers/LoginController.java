139795dc91266b3cc93c066cca958a5d0f8c89c5
Jonathan Butler <xxbutler86xx@gmail.com>
2024-03-13 17:23:31 -0400

    Create project directory structure and add java class files / fxml files.

--- src/main/java/com/jgm/securepasswordmanager/controllers/LoginController.java
+++ src/main/java/com/jgm/securepasswordmanager/controllers/LoginController.java
@@ -3,7 +3,7 @@ package com.jgm.securepasswordmanager.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class MainWindowController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
