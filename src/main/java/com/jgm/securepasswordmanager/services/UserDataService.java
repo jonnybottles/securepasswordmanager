75d9f9abe5e9d3b75ea91589efd4f7a1f136ffab
Jonathan Butler <xxbutler86xx@gmail.com>
2024-03-16 08:45:52 -0400

    Create test and validate that createUserDataDirectory method works successfully.

--- src/main/java/com/jgm/securepasswordmanager/services/UserDataService.java
+++ src/main/java/com/jgm/securepasswordmanager/services/UserDataService.java
@@ -11,16 +11,16 @@ public class UserDataService {
     // Creates the user_data directory if it doesn't already exist
     public boolean createUserDataDirectory() {
 
-        // Obtain the path to the users current working directory / where the program was exeuted from.
+        // Obtain the path to the users current working directory / where the program was executed from.
         String currentPath = System.getProperty("user.dir");
 
         // Creates user_data directory path by concating current path
         // with OS specific separator (e.g. / or \) and the user data directory
-        File userDirectory = new File(currentPath + File.separator + "user_data");
+        File userDataDirectory = new File(currentPath + File.separator + "user_data");
 
-        if (!userDirectory.exists()) {
+        if (!userDataDirectory.exists()) {
             try {
-                boolean wasCreated = userDirectory.mkdirs();
+                boolean wasCreated = userDataDirectory.mkdirs();
                 if (!wasCreated) {
                     return false;
                 }
--- src/test/java/com/jgm/securepasswordmanager/services/UserDataServiceTest.java
+++ src/test/java/com/jgm/securepasswordmanager/services/UserDataServiceTest.java
@@ -3,12 +3,36 @@ package com.jgm.securepasswordmanager.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDataServiceTest {
 
+    private UserDataService theUserDataService ;
+
     @BeforeEach
     public void setUp() {
-        UserDataService theUserDataService = new UserDataService();
+         theUserDataService = new UserDataService();
     }
+
+
+    @Test
+    public void testCreateUserDataDirectory() {
+
+        File userDataDirectory = new File("user_data");
+        assertFalse(userDataDirectory.exists());
+
+        boolean result = theUserDataService.createUserDataDirectory();
+
+        // Assert
+        assertTrue(result); // Directory should be created successfully.
+        assertTrue(userDataDirectory.exists()); // Directory should exist after creation
+
+        // Clean up
+        userDataDirectory.delete(); // Delete the created directory after the test
+
+    }
+
+
 }
