2495602404b00a2e132e2fa7a3ce9b4c0eec28e3
Jonathan Butler <xxbutler86xx@gmail.com>
2024-03-16 08:13:06 -0400

    Add createUserDataDirectory method inside of UserDataService class to allow for creating the user_data directory if it does not already exist.

--- src/main/java/com/jgm/securepasswordmanager/datamodel/User.java
+++ src/main/java/com/jgm/securepasswordmanager/datamodel/User.java
@@ -1,4 +1,60 @@
 package com.jgm.securepasswordmanager.datamodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private List<WebsiteCredential> websiteCredentialList;
+
+    public User(String firstName, String lastName, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.websiteCredentialList = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<WebsiteCredential> getWebsiteCredentialList() {
+        return websiteCredentialList;
+    }
+
+    public void setWebsiteCredentialList(List<WebsiteCredential> websiteCredentialList) {
+        this.websiteCredentialList = websiteCredentialList;
+    }
 }
--- src/main/java/com/jgm/securepasswordmanager/services/UserDataService.java
+++ src/main/java/com/jgm/securepasswordmanager/services/UserDataService.java
@@ -1,4 +1,37 @@
 package com.jgm.securepasswordmanager.services;
 
+import java.io.File;
+
 public class UserDataService {
+
+
+    public UserDataService() {
+    }
+
+    // Creates the user_data directory if it doesn't already exist
+    public boolean createUserDataDirectory() {
+
+        // Obtain the path to the users current working directory / where the program was exeuted from.
+        String currentPath = System.getProperty("user.dir");
+
+        // Creates user_data directory path by concating current path
+        // with OS specific separator (e.g. / or \) and the user data directory
+        File userDirectory = new File(currentPath + File.separator + "user_data");
+
+        if (!userDirectory.exists()) {
+            try {
+                boolean wasCreated = userDirectory.mkdirs();
+                if (!wasCreated) {
+                    return false;
+                }
+            } catch (SecurityException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+
 }
