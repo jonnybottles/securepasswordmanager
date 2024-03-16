f4f8399206a42d8eeb9ac39cc13c0197bef00daa
Jonathan Butler <xxbutler86xx@gmail.com>
2024-03-16 08:36:16 -0400

    Add test directory structure.

---
+++ src/test/java/com/jgm/securepasswordmanager/services/UserDataServiceTest.java
@@ -0,0 +1,14 @@
+package com.jgm.securepasswordmanager.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDataServiceTest {
+
+    @BeforeEach
+    public void setUp() {
+        UserDataService theUserDataService = new UserDataService();
+    }
+}
