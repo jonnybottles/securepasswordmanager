755f3cea4690431f69d9d69e3ac12c7226cf4325
Jonathan Butler <xxbutler86xx@gmail.com>
2024-03-16 06:05:41 -0400

    Complete Encryption Service Class

--- src/main/java/com/jgm/securepasswordmanager/datamodel/WebsiteCredential.java
+++ src/main/java/com/jgm/securepasswordmanager/datamodel/WebsiteCredential.java
@@ -1,4 +1,19 @@
 package com.jgm.securepasswordmanager.datamodel;
 
 public class WebsiteCredential {
+    private String webSiteName;
+    private String webSiteUserName;
+    private String webSitePassword;
+    private String notes;
+
+    public WebsiteCredential(String webSiteName, String webSiteUserName, String webSitePassword, String notes) {
+        this.webSiteName = webSiteName;
+        this.webSiteUserName = webSiteUserName;
+        this.webSitePassword = webSitePassword;
+        this.notes = notes;
+    }
+
+
+
+
 }
